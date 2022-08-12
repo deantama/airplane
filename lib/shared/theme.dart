3aba44e5e6d7c9f052be7ac46418ddbf6adb2c36
Dean Pratama Putra <85298439+deantama@users.noreply.github.com>
2022-08-12 23:16:30 +0700

    Aplikasi Travel Pesawat

---
+++ lib/shared/theme.dart
@@ -0,0 +1,43 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaulMargin = 24.0;
+double defaultRadius = 17.0;
+
+Color kPrimaryColor = Color(0xff5C40CC);
+Color kBlackColor = Color(0xff1F1449);
+Color kWhiteColor = Color(0xffFFFFFF);
+Color kGreyColor = Color(0xff9698A9);
+Color kRedColor = Color(0xffEB70A5);
+Color kGreenColor = Color(0xff0EC3AE);
+Color kBackgroudColor = Color(0xffFAFAFA);
+Color kInactiveColor = Color(0xffDBD7EC);
+
+TextStyle BlackStyleColor = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+TextStyle WhiteStyleColor = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle GreyStyleColor = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+TextStyle RedStyleColor = GoogleFonts.poppins(
+  color: kRedColor,
+);
+TextStyle GreenStyleColor = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+TextStyle PurpleStyleColor = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
